d6878e767e2485ffef37e99c159acc0bd32c8fbc
Rupayan Saha <rup130104@gmail.com>
2024-07-07 21:51:06 +0530

    first

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+public class RewardValue {
+    private final double cashValue;
+    public static final double RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = ToCash(milesValue);
+    }
+    
+
+    private static int ToMiles(double cashValue) {
+        return (int) (cashValue / RATE);
+    }
+
+    private static double ToCash(int milesValue) {
+        return milesValue * RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return ToMiles(this.cashValue);
+    }
+}
